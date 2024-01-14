d3ff2b7400e65ba60835bd3e70247eafb347a954
Alisson Cruz <alissonpdc@gmail.com>
2024-01-14 19:50:15 -0300

    deleted features

--- bucket-remote-state/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alissonpdc_terraform_remote_state_bucket_arn" {
-  description = "Terraform Remote State S3 Bucket ARN"
-  value       = aws_s3_bucket.alissonpdc_terraform_remote_state_bucket.arn
-}
-
-output "alissonpdc_terraform_remote_state_bucket_id" {
-  description = "Terraform Remote State S3 Bucket ID"
-  value       = aws_s3_bucket.alissonpdc_terraform_remote_state_bucket.id
-}
