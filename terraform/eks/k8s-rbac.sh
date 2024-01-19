8405741119ceed81712f9c33c74c3f5d5c44d59b
Alisson Cruz <alissonpdc@gmail.com>
2024-01-19 11:46:28 -0300

    fix: k8s rbac

--- terraform/eks/k8s-rbac.sh
+++ terraform/eks/k8s-rbac.sh
@@ -12,12 +12,12 @@ kubectl apply -f ./eks/files/eks-full-access.yaml
 echo "==> Adding new role to auth-map"
 eksctl create iamidentitymapping --cluster eks-cluster --region=us-east-1 \
 --arn arn:aws:iam::211125453026:role/AWSReservedSSO_AdministratorAccess_018645c6c1d0e0fc \
---username "{{SessionName}}" --group cluster-admin --no-duplicate-arns
+--username "{{SessionName}}" --group eks-full-access-group --no-duplicate-arns
 
 echo "==> Adding new user to auth-map"
 eksctl create iamidentitymapping --cluster eks-cluster --region=us-east-1 \
 --arn arn:aws:iam::211125453026:user/console \
---username admin --group cluster-admin --no-duplicate-arns
+--username admin --group eks-full-access-group --no-duplicate-arns
 
 echo "==> Printing updated auth-map"
 eksctl get iamidentitymapping --cluster eks-cluster --region=us-east-1
